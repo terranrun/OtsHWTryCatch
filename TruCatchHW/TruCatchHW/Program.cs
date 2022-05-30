685499b19491be5d85c06a854ca2208c68b51fa2
Mikhail VV <esto045@gmail.com>
2022-05-30 14:09:21 +0300

    +

---
+++ TruCatchHW/TruCatchHW/Program.cs
@@ -0,0 +1,51 @@
+﻿Calculate();
+
+
+void Calculate()
+{
+    do
+    {
+        List<object> values = GetNumber("Пожайлуйста, введите желаемые значения через пробел : ") ;
+
+        foreach(var item in values)
+        {
+            Console.WriteLine(item);
+        }
+       
+
+    } while (AskContinue());
+}
+
+bool AskContinue()
+{
+    while (true)
+    {
+        Console.WriteLine("не хочешь продолжать? напиши STOP");
+        var status = Console.ReadLine();
+        if (status == "STOP" || status == "stop" || status == "Stop")
+            return false;     
+        else
+        {
+            Console.WriteLine("Продолжаем) ");
+            return true;
+        }
+        
+    }
+}
+
+int GetResult(int x, int y, string? cmd)
+{
+    throw new NotImplementedException();
+}
+
+List<object> GetNumber(string text)
+{
+    Console.WriteLine(text);
+    int x = Int32.Parse(Console.ReadLine());
+    var cmd = Console.ReadLine(); 
+    int y = Int32.Parse(Console.ReadLine());
+    List<object> str = new List<object> { x , cmd, y} ;
+
+    return str;
+      
+}
